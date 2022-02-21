886f8d1c5dc2a463325cd2c8b73c494279072faa
mal <mal@kali>
2022-02-20 20:37:06 -0600

    update redir url

--- example-ui/src/environments/environment.prod.ts
+++ example-ui/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   oktaConfig : {
     clientId: '0oa3xjxfg1traSvHU5d7',
     issuer: `https://dev-4699488.okta.com/oauth2/default`,
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://willstatictest.adesaauctionoperations.com//login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true
   }
